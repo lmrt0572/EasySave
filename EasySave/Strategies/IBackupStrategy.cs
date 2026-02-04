f430964fb27aa0050737af9dc73ce8ca56b6cdb0
Bastienldx <bastien.ledoux@viacesi.fr>
2026-02-04 21:04:03 +0100

    feat: add Backup strategy

--- EasySave/Strategies/IBackupStrategy.cs
+++ EasySave/Strategies/IBackupStrategy.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using EasySave.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EasySave.Strategies
 {
-    internal interface IBackupStrategy
+    public interface IBackupStrategy
     {
+        void Execute(BackupJob job, Action<string, string, long, long> onFileCompleted);
     }
 }
