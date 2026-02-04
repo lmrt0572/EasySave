bf0a16c86816bb94480c920e71e5bc208f857fb3
Martin <leo.martin1@viacesi.fr>
2026-02-04 15:38:15 +0100

    feat: implement FileUtils with .NET 10 optimizations

--- EasySave/Utils/FileUtils.cs
+++ EasySave/Utils/FileUtils.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace EasySave.Utils
 {
-    internal class FileUtils
+    public static class FileUtils
     {
+        public static void CopyFile(string source, string dest)
+        {
+            string directory = Path.GetDirectoryName(dest);
+            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+            File.Copy(source, dest, true);
+        }
+
+        public static string GetUNCPath(string path) => Path.GetFullPath(path);
+
+        public static long GetFileSize(string path) => new FileInfo(path).Length;
+
+        public static IEnumerable<string> GetAllFilesRecursive(string directory)
+            => Directory.EnumerateFiles(directory, "*", SearchOption.AllDirectories);
+
+        public static bool DirectoryExists(string path) => Directory.Exists(path);
     }
 }
+
